00c29ff6a3e837a1355570022baa432338b120e6
DamienSerrurier <damien.ser@laposte.net>
2021-02-12 20:35:30 +0100

    Ajout méthode updatePatien

--- Partie-2-Ecrire les données/Models/Patients.php
+++ Partie-2-Ecrire les données/Models/Patients.php
@@ -109,4 +109,31 @@ class Patients extends Database
             return false;
         }
     }
+
+    public function addOnePatientInformation($id)
+    {
+        $query = "SELECT * FROM `patients` WHERE `id` = :id";
+        $buildQuery = parent::getDatabase()->prepare($query);
+        $buildQuery->bindValue('id', $id, PDO::PARAM_INT);
+        $buildQuery->execute();
+        $resultQuery = $buildQuery->fetch();
+        if (!empty($resultQuery)) {
+            return $resultQuery;
+        } else {
+            return false;
+        }
+    }
+
+    public function updatePatient($arrayParameters)
+    {
+        $query = "UPDATE `patients` SET `lastname` = :lastname, `firstname` = :firstname, `birthdate` = :birthdate `phone` = :phone,`mail` = :mail WHERE `id` = :id";
+        $buildQuery = parent::getDatabase()->prepare($query);
+        $buildQuery->bindValue('id', $arrayParameters['id'], PDO::PARAM_INT);
+        $buildQuery->bindValue('lastname', $arrayParameters['lastname'], PDO::PARAM_STR);
+        $buildQuery->bindValue('firstname', $arrayParameters['firstname'], PDO::PARAM_STR);
+        $buildQuery->bindValue('birthdate', $arrayParameters['birthdate'], PDO::PARAM_STR);
+        $buildQuery->bindValue('phone', $arrayParameters['phone'], PDO::PARAM_STR);
+        $buildQuery->bindValue('mail', $arrayParameters['mail'], PDO::PARAM_STR);
+        return $buildQuery->exectute();
+    }
 }
