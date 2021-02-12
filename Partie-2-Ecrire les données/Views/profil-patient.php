9b7f309eec45dcf303cf7a4af2c6aa4fdc069a8b
DamienSerrurier <damien.ser@laposte.net>
2021-02-13 00:52:59 +0100

    Ajout d'un bouton modifier un patient

--- Partie-2-Ecrire les données/Views/profil-patient.php
+++ Partie-2-Ecrire les données/Views/profil-patient.php
@@ -13,20 +13,32 @@ require '../Controllers/profil-patient-controller.php';
 </head>
 
 <body>
+
+    <?php
+    if (isset($_SESSION['successMessage']) && $_SESSION['successMessage'] != NULL) {
+    ?>
+        <div>
+            <p style="background-color: green;"><?= $_SESSION['successMessage'] ?></p>
+        </div>
+    <?php
+        $_SESSION['successMessage'] = NULL;
+    }
+    ?>
+
     <ul>
-        <li>N° du patient : <?= isset($patientInformation['id']) ? $patientInformation['id'] : $errorMessage ?></li>
-        <li>Nom de famille du patient : <?= isset($patientInformation['lastname']) ? strtoupper($patientInformation['lastname']) : $errorMessage ?></li>
-        <li>Prénom du patient : <?= isset($patientInformation['lastname']) ? $patientInformation['firstname'] : $errorMessage ?></li>
-        <li>Date de naissance du patient : <?= isset($patientInformation['lastname']) ? strftime('%d, %m, %Y', strtotime($patientInformation['birthdate'])) : $errorMessage ?></li>
-        <li>N° de téléphone du patient : <?= isset($patientInformation['lastname']) ? $patientInformation['phone'] : $errorMessage ?></li>
-        <li>Adresse émail du patient : <?= isset($patientInformation['lastname']) ? $patientInformation['mail'] : $errorMessage ?></li>
+        <li>N° du patient : <?= isset($patientInformations['id']) ? $patientInformations['id'] : $errorMessage ?></li>
+        <li>Nom de famille du patient : <?= isset($patientInformations['lastname']) ? strtoupper($patientInformations['lastname']) : $errorMessage ?></li>
+        <li>Prénom du patient : <?= isset($patientInformations['lastname']) ? $patientInformations['firstname'] : $errorMessage ?></li>
+        <li>Date de naissance du patient : <?= isset($patientInformations['lastname']) ? strftime('%d, %m, %Y', strtotime($patientInformations['birthdate'])) : $errorMessage ?></li>
+        <li>N° de téléphone du patient : <?= isset($patientInformations['lastname']) ? $patientInformations['phone'] : $errorMessage ?></li>
+        <li>Adresse émail du patient : <?= isset($patientInformations['lastname']) ? $patientInformations['mail'] : $errorMessage ?></li>
     </ul>
     <div>
         <a href="liste-patients.php">Retour</a>
     </div>
     <div>
         <form action="modification-patient.php" method="post">
-            <button type="submit" name="updatePatient" value="<?= isset($patientInformation['id']) ? $patientInformation['id'] : '' ?>">Modifier un patient</button>
+            <button type="submit" name="modifyPatientId" value="<?= isset($patientInformations['id']) ? $patientInformations['id'] : '' ?>">Modifier un patient</button>
         </form>
     </div>
 </body>
