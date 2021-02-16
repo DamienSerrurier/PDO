523c9578ada6e562c0345eb8a15bb6133a4c55ad
DamienSerrurier <damien.ser@laposte.net>
2021-02-16 12:30:51 +0100

    Ajout rendez-vous-controller

---
+++ Partie-2-Ecrire les données/Controllers/rendez-vous-controller.php
@@ -0,0 +1,23 @@
+<?php
+setlocale(LC_ALL, 'fr_FR.utf8');
+
+require '../Models/Database.php';
+require '../Models/Patients.php';
+
+$patient = new Patients();
+
+if (isset($_GET['idAppointment']) && !empty($_GET['idAppointment'])) {
+
+    $id = htmlspecialchars($_GET['idAppointment']);
+
+    $regexId = '/^[0-9]+$/';
+
+    if (preg_match($regexId, $id)) {
+        $verifiedId = (int)$id;
+        $appointmentInformations =  $patient->getOneAppointmentWithPatientInformationns($verifiedId);
+        $dateHourExplode = explode(' ', $appointmentInformations['dateHour']);
+        $formatedDate = strftime('%A %d %B %Y', strtotime($dateHourExplode[0]));
+    } else {
+        $errorMessage = 'Arrête de jouer avec mon URL!';
+    }
+}
