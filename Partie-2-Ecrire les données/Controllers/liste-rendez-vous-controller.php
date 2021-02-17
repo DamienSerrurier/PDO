ac490bd1e8495cfe99e35c865aa6654da05bc4b1
DamienSerrurier <damien.ser@laposte.net>
2021-02-17 22:11:55 +0100

    Ajout code pour la suppression de rendez-vous

--- Partie-2-Ecrire les données/Controllers/liste-rendez-vous-controller.php
+++ Partie-2-Ecrire les données/Controllers/liste-rendez-vous-controller.php
@@ -4,5 +4,24 @@ setlocale(LC_ALL, 'fr_FR.utf8');
 require '../Models/Database.php';
 require '../Models/Patients.php';
 
+
+if (isset($_POST['deleteAppointment']) && !empty($_POST['deleteAppointment'])) {
+
+    require '../Models/Rendez-vous.php';
+
+    $appointment = new Appointments();
+
+    $id = htmlspecialchars($_POST['deleteAppointment']);
+
+    $regexId = '/^[0-9]+$/';
+
+    if (preg_match($regexId, $id)) {
+        $verifiedId = (int)$id;
+        $resultDeleteAppointment =  $appointment->deleteAppointmentById($verifiedId);
+    } else {
+        $errorMessage = 'Arrête de jouer avec mes $_POST!';
+    }
+}
+
 $patients = new Patients;
 $allAppointments = $patients->getAllAppointmentsWhisAllPatientsInformations();
