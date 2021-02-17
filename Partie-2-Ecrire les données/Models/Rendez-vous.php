9fcb25b7d0dc84cac62d4ed6a6bff03dbacad0e6
DamienSerrurier <damien.ser@laposte.net>
2021-02-17 13:29:19 +0100

    Ajouts dune méthode permettant de récupérer toutes les informations d'un rendez-vous par son id et d'une méthode qui permet la modification d'un rendez-vous

--- Partie-2-Ecrire les données/Models/Rendez-vous.php
+++ Partie-2-Ecrire les données/Models/Rendez-vous.php
@@ -54,12 +54,52 @@ class Appointments extends Database
      * @param array
      * @param boolean
      */
-    public function addAppointment($arrayParameters)
+    public function addAppointment(array $arrayParameters)
     {
-        $query = "INSERT INTO `appointments` (`dateHour`, `idPatients`) VALUES (:dateHour, :idPatients);";
-        $buildQuery = parent::getDataBase()->prepare($query);
+        $query = "INSERT INTO `appointments` (`dateHour`, `idPatients`) VALUES (:dateHour, :idPatient);";
+        $buildQuery = parent::getDatabase()->prepare($query);
         $buildQuery->bindValue('dateHour', $arrayParameters['dateHour'], PDO::PARAM_STR);
-        $buildQuery->bindValue('idPatients', $arrayParameters['idPatients'], PDO::PARAM_INT);
+        $buildQuery->bindValue('idPatient', $arrayParameters['idPatient'], PDO::PARAM_INT);
+        return $buildQuery->execute();
+    }
+
+    /**
+     * Méthode qui permet de récupérer toutes les informations d'un rendez-vous par son id
+     * 
+     * @param int
+     * @return array|boolean
+     */
+    public function getOneAppointmentById(int $id)
+    {
+        $query = "SELECT `id`, `dateHour`, `idPatients`
+            FROM `appointments`
+            WHERE `id` = :id;";
+        $buildQuery = parent::getDatabase()->prepare($query);
+        $buildQuery->bindValue('id', $id, PDO::PARAM_INT);
+        $buildQuery->execute();
+        $resultQuery = $buildQuery->fetch(PDO::FETCH_ASSOC);
+        if (!empty($resultQuery)) {
+            return $resultQuery;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Méthode qui permet la modification d'un rendez-vous
+     * 
+     * @param array
+     * @return boolean
+     */
+    public function updateAppointment($arrayParameters)
+    {
+        $query = "UPDATE `appointments` 
+            SET `dateHour` = :dateHour, `idPatients` = :idPatient 
+            WHERE `id` = :idAppointment;";
+        $buildQuery = parent::getDatabase()->prepare($query);
+        $buildQuery->bindValue('dateHour', $arrayParameters['dateHour'], PDO::PARAM_STR);
+        $buildQuery->bindValue('idPatient', $arrayParameters['idPatient'], PDO::PARAM_INT);
+        $buildQuery->bindValue('idAppointment', $arrayParameters['idAppointment'], PDO::PARAM_INT);
         return $buildQuery->execute();
     }
 }
