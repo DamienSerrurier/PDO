8eab7e5cc38271d2b89c6a9da08e22cadf4fbf19
DamienSerrurier <damien.ser@laposte.net>
2021-02-17 22:16:23 +0100

    Ajout du code pour l'affichage des rendez-vous attaché par l'id du patient

--- Partie-2-Ecrire les données/Controllers/profil-patient-controller.php
+++ Partie-2-Ecrire les données/Controllers/profil-patient-controller.php
@@ -1,10 +1,13 @@
 <?php
+setlocale(LC_ALL, 'fr_FR.utf8');
 session_start();
 
 require '../Models/Database.php';
 require '../Models/Patients.php';
+require '../Models/Rendez-vous.php';
 
 $patient = new Patients();
+$appointments = new Appointments();
 
 if ((isset($_GET['idPatient'])) && !empty($_GET['idPatient'])) {
 
@@ -15,6 +18,8 @@ if ((isset($_GET['idPatient'])) && !empty($_GET['idPatient'])) {
     if (preg_match($regexId, $id)) {
         $verifiedId = (int)$id;
         $patientInformations =  $patient->getOnePatientInformation($verifiedId);
+        $patienttAppointmentsInformations = $appointments->getAllAppointmentsByPatientId($verifiedId);
+        var_dump($patienttAppointmentsInformations);
     } else {
         $errorMessage = 'Arrête de jouer avec mon URL!';
     }
