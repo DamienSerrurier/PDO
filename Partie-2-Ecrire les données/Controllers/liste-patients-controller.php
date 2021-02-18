b6c991562a6a21af84bbf4b40ef008f7adfb3309
DamienSerrurier <damien.ser@laposte.net>
2021-02-18 21:29:31 +0100

    Ajout du code de pagination

--- Partie-2-Ecrire les données/Controllers/liste-patients-controller.php
+++ Partie-2-Ecrire les données/Controllers/liste-patients-controller.php
@@ -7,7 +7,6 @@ $patients = new Patients();
 if (isset($_POST['deletePatient']) && !empty($_POST['deletePatient'])) {
 
     $id = htmlspecialchars($_POST['deletePatient']);
-
     $regexId = '/^[0-9]+$/';
 
     if (preg_match($regexId, $id)) {
@@ -19,16 +18,32 @@ if (isset($_POST['deletePatient']) && !empty($_POST['deletePatient'])) {
     }
 }
 
+$countPatients = $patients->countPatients();
+
 if (isset($_POST['searchPatient'])) {
 
     $search = htmlspecialchars($_POST['search']);
     $querySearch = '%' . $search . '%';
     $patientsInformations = $patients->searchPatient($querySearch);
 } else {
-    $patientsInformations = $patients->getPatientsInformations();
+
+    if (!isset($_GET['page'])) {
+
+        header('Location: liste-patients.php?page=1');
+    } else {
+        $regexPage = '/^[0-9]+$/';
+        $actualPage = htmlspecialchars($_GET['page']);
+
+        if (preg_match($regexPage, $actualPage)) {
+
+            $almostTotalPages = (int)$countPatients['countPatients'] / 10; // Calcul du nombre de pages
+            $totalPages = ceil($almostTotalPages); // Arrondir à l'entier supérieur
+            $startValue = ($actualPage - 1) * 10; // Déterminer la valeur de départ
+            $patientsInformations = $patients->getPatientsPaginate($startValue);
+        }
+    }
 }
 
-
-if (!$patientsInformations) {
+if (!empty($patientsInformations)) {
     $errorMessage = 'Il y a eu un problème lors de la récupération des données sur les patients.';
 }
